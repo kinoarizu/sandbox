5478c4776129444209965f328a730ffc7db751be
ルイタイゼン <abuaziscorp@gmail.com>
2024-06-08 12:27:15 +0700

    Add ListView.builder to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,61 @@ class MyApp extends StatelessWidget {
           title: const Text("Flutter Sandbox"),
           backgroundColor: Colors.amber[300],
         ),
-        body: const Center(
-          child: Text("Hello World!"),
+        body: Center(
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return ListTile(
+                title: item.buildTitle(context),
+                subtitle: item.buildSubtitle(context),
+              );
+            },
+          ),
         ),
       ),
     );
   }
+}
+
+final items = List<ListItem>.generate(
+  1000,
+  (i) => i % 6 == 0
+      ? HeadingItem('Heading $i')
+      : MessageItem('Sender $i', 'Message body $i'),
+);
+
+abstract class ListItem {
+  Widget buildTitle(BuildContext context);
+  Widget buildSubtitle(BuildContext context);
+}
+
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headlineSmall,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+  
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
 }
